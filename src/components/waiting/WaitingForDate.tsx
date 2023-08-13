ecfab583da57830bfad1b678dd27d6f05f7c0298
Elliot Potter <elliotpotter496@gmail.com>
2023-08-13 11:34:34 -0700

    Formatted the waiting screen

--- src/components/waiting/WaitingForDate.tsx
+++ src/components/waiting/WaitingForDate.tsx
@@ -16,9 +16,12 @@ export function WaitingForDate(props: PropTypes) {
     }
 
     return (
-        <div>
-            <p>You have a date scheduled with {(otherUser as UserMini).firstName}{isSetup ? "'s friend" : ""} on {(new Date(lockingDate.time)).toDateString()}</p>
-            <p>You can match with more people when you have gone on that date and reviewed it.</p>
+        <div style={{display: "flex", justifyContent: "center", flexDirection: "column", height: "100vh"}}>
+            <div style={{maxWidth: "calc(100vw - 20)", maxHeight: "calc(100vh - 20)", textAlign: "center", margin: 10}}>
+                <p style={{fontSize: 30}}>Time for a Real Connection!</p>
+                <p style={{marginTop: 10}}>You have a date scheduled with {(otherUser as UserMini).firstName}{isSetup ? "'s friend" : ""} on {(new Date(lockingDate.time)).toDateString()}.</p>
+                <p style={{marginTop: 10}}>You can match with more people when you have gone on that date and reviewed it.</p>
+            </div>
         </div>
     )
 }
