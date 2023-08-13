09d3aa9a4dca62c4d2bff5d30c3975b30a9e40d3
Elliot Potter <elliotpotter496@gmail.com>
2023-08-13 14:20:58 -0700

    DateReview is pretty good-looking

--- src/components/recording_date/DateReview.tsx
+++ src/components/recording_date/DateReview.tsx
@@ -1,9 +1,19 @@
 import {useState} from "react";
 import {useReviewDateMutation} from "../../services/api";
-import {Slider, Toggle} from "rsuite";
-const DEFAULT_SLIDER_VALUE = 5.5
+import Slider from "rsuite/Slider";
+import Toggle from "rsuite/Toggle";
+import "rsuite/dist/rsuite.css"
+const DEFAULT_SLIDER_VALUE = 5
 const MIN_SLIDER_VALUE = 1
 const MAX_SLIDER_VALUE = 10
+
+const toggleContainerGroup = {backgroundColor: "rgb(243,244,246)", padding: 10, marginTop: 10, marginBottom: 20,
+    borderRadius: 15
+}
+const toggleDivStyle = {marginTop: 20, marginBottom: 20, marginLeft: 10, marginRight: 10}
+const toggleTextStyle = {marginLeft: 10, fontSize: 16}
+const sliderDivStyle = {}
+const sliderTextStyle = {}
 
 interface PropTypes {
     user: User
@@ -34,119 +44,138 @@ export function DateReview(props: PropTypes) {
     }
 
     return (
-        <div>
-            <div>Please review your date with {(otherUser as UserMini).firstName}{isSetup ? "'s friend" : ""}</div>
-
-            <div>
-                <label>
-                    <Toggle
-                        defaultChecked={!wasNoShow}
-                        onChange={() => {
-                            setWasNoShow(!wasNoShow)
-                        }}
-                    />
-                    <span>Did your date show up?</span>
-                </label>
-            </div>
-
-            <div>
-                <label>
-                    <Toggle
-                        defaultChecked={!wasCatfish}
-                        onChange={() => {
-                            setWasCatfish(!wasCatfish)
-                        }}
-                    />
-                    <span>Did your date look like their photos?</span>
-                </label>
-            </div>
-
-            <div>
-                <label>
-                    <Toggle
-                        defaultChecked={!wasThreatening}
-                        onChange={() => {
-                            setWasThreatening(!wasThreatening)
-                        }}
-                    />
-                    <span>Did you feel safe around your date?</span>
-                </label>
-            </div>
-
-            <div>
-                <div>How intelligent did you find your date?</div>
-                <Slider
-                    defaultValue={intelligent}
-                    min={MIN_SLIDER_VALUE}
-                    max={MAX_SLIDER_VALUE}
-                    graduated
-                    progress
-                    renderMark={mark => mark}
-                    onChange={(val) => setIntelligent(val)}
-                />
-            </div>
-
-            <div>
-                <div>How trustworthy did you find your date?</div>
-                <Slider
-                    defaultValue={trustworthy}
-                    min={MIN_SLIDER_VALUE}
-                    max={MAX_SLIDER_VALUE}
-                    graduated
-                    progress
-                    renderMark={mark => mark}
-                    onChange={(val) => setTrustworthy(val)}
-                />
-            </div>
-
-            <div>
-                <div>How attractive did you find your date?</div>
-                <Slider
-                    defaultValue={attractive}
-                    min={MIN_SLIDER_VALUE}
-                    max={MAX_SLIDER_VALUE}
-                    graduated
-                    progress
-                    renderMark={mark => mark}
-                    onChange={(val) => setAttractive(val)}
-                />
-            </div>
-
-            <div>
-                <div>How satisfied were you with your date overall?</div>
-                <Slider
-                    defaultValue={satisfied}
-                    min={MIN_SLIDER_VALUE}
-                    max={MAX_SLIDER_VALUE}
-                    graduated
-                    progress
-                    renderMark={mark => mark}
-                    onChange={(val) => setSatisfied(val)}
-                />
-            </div>
-
-            <div>
-                <label>
-                    <Toggle
-                        defaultChecked={secondDate}
-                        onChange={() => {
-                            setSecondDate(!secondDate)
-                        }}
-                    />
-                    <span>Would you go on a second date with this person?</span>
-                </label>
-            </div>
-
-            <div onClick={() => {
-              let review: DateReview = {
-                  reviewee: (otherUser as UserMini)._id, dateObject: lockingDate._id,
-                  wasNoShow, wasCatfish, wasThreatening,
-                  intelligent, trustworthy, attractive, pleasant, satisfied, secondDate,
-              }
-
-              reviewDate({cognitoId: props.user.cognitoId, review})
+        <div style={{width: "100vw", display: "flex", justifyContent: "center", overflow: "scroll", maxHeight: "100vh"}}>
+            <div style={{maxWidth: "100vw", width: 500,
+                padding: 20,
             }}>
-                {"Finish the survey >>"}
+                <div style={{fontSize: 25, textAlign: "center"}}>
+                    Please review your date with {(otherUser as UserMini).firstName}{isSetup ? "'s friend" : ""}
+                </div>
+
+                <div style={toggleContainerGroup}>
+                    <div style={toggleDivStyle}>
+                        <label>
+                            <Toggle
+                                defaultChecked={!wasNoShow}
+                                onChange={() => {
+                                    setWasNoShow(!wasNoShow)
+                                }}
+                            />
+                            <span style={toggleTextStyle}>Did your date show up?</span>
+                        </label>
+                    </div>
+
+                    <div style={toggleDivStyle}>
+                        <label>
+                            <Toggle
+                                defaultChecked={!wasCatfish}
+                                onChange={() => {
+                                    setWasCatfish(!wasCatfish)
+                                }}
+                            />
+                            <span style={toggleTextStyle}>Did your date look like their photos?</span>
+                        </label>
+                    </div>
+
+                    <div style={toggleDivStyle}>
+                        <label>
+                            <Toggle
+                                defaultChecked={!wasThreatening}
+                                onChange={() => {
+                                    setWasThreatening(!wasThreatening)
+                                }}
+                            />
+                            <span style={toggleTextStyle}>Did you feel safe around your date?</span>
+                        </label>
+                    </div>
+                </div>
+
+                <div style={{
+                    padding: 15
+                }}>
+                    <div>
+                        <div>How intelligent did you find your date?</div>
+                        <Slider
+                            defaultValue={intelligent}
+                            min={MIN_SLIDER_VALUE}
+                            max={MAX_SLIDER_VALUE}
+                            graduated
+                            progress
+                            renderMark={mark => mark}
+                            onChange={(val) => setIntelligent(val)}
+                        />
+                    </div>
+
+                    <div>
+                        <div>How trustworthy did you find your date?</div>
+                        <Slider
+                            defaultValue={trustworthy}
+                            min={MIN_SLIDER_VALUE}
+                            max={MAX_SLIDER_VALUE}
+                            graduated
+                            progress
+                            renderMark={mark => mark}
+                            onChange={(val) => setTrustworthy(val)}
+                        />
+                    </div>
+
+                    <div>
+                        <div>How attractive did you find your date?</div>
+                        <Slider
+                            defaultValue={attractive}
+                            min={MIN_SLIDER_VALUE}
+                            max={MAX_SLIDER_VALUE}
+                            graduated
+                            progress
+                            renderMark={mark => mark}
+                            onChange={(val) => setAttractive(val)}
+                        />
+                    </div>
+
+                    <div>
+                        <div>How satisfied were you with your date overall?</div>
+                        <Slider
+                            defaultValue={satisfied}
+                            min={MIN_SLIDER_VALUE}
+                            max={MAX_SLIDER_VALUE}
+                            graduated
+                            progress
+                            renderMark={mark => mark}
+                            onChange={(val) => setSatisfied(val)}
+                        />
+                    </div>
+                </div>
+
+                <div style={toggleContainerGroup}>
+                    <div style={toggleDivStyle}>
+                        <label>
+                            <Toggle
+                                defaultChecked={secondDate}
+                                onChange={() => {
+                                    setSecondDate(!secondDate)
+                                }}
+                            />
+                            <span style={toggleTextStyle}>Would you go on a second date with this person?</span>
+                        </label>
+                    </div>
+                </div>
+
+                <div
+                    style={{fontSize: 20, cursor: "pointer", textAlign: "center",
+                        marginBottom: 40
+                    }}
+
+                    onClick={() => {
+                      let review: DateReview = {
+                          reviewee: (otherUser as UserMini)._id, dateObject: lockingDate._id,
+                          wasNoShow, wasCatfish, wasThreatening,
+                          intelligent, trustworthy, attractive, pleasant, satisfied, secondDate,
+                      }
+
+                      reviewDate({cognitoId: props.user.cognitoId, review})
+                }}>
+                    {"Finish the survey >>"}
+                </div>
             </div>
         </div>
     )
