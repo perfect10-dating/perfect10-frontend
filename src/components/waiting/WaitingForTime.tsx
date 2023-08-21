2331067ebc00e9af3f3ff4d59f070bcaffb923fe
Elliot Potter <elliotpotter496@gmail.com>
2023-08-20 20:51:38 -0400

    maybe prevent infinite looping when unlocking time?

--- src/components/waiting/WaitingForTime.tsx
+++ src/components/waiting/WaitingForTime.tsx
@@ -9,22 +9,26 @@ export function WaitingForTime(props: PropTypes) {
 
     const [time, setTime] = useState(Date.now);
 
+    let millisecondsUntilUnlock = (new Date(props.user.unlockTime as string)).getTime() - time
+
     useEffect(() => {
         const interval = setInterval(() => {
-            setTime(Date.now);
+            // don't keep updating time if we're unlocking
+            if (millisecondsUntilUnlock >= 0) {
+                setTime(Date.now);
+            }
         }, 1000);
 
         return () => clearInterval(interval);
     }, []);
 
-    let millisecondsUntilUnlock = (new Date(props.user.unlockTime as string)).getTime() - time
     let seconds = Math.floor(millisecondsUntilUnlock / 1000)
     let minutes = Math.floor(seconds / 60)
     let hours = Math.floor(minutes / 60)
     let days = Math.floor(hours / 24)
 
     // if lock time expires, attempt to unlock the user
-    if (millisecondsUntilUnlock < 0) {
+    if (millisecondsUntilUnlock <= 0) {
         unlockUser()
     }
 
