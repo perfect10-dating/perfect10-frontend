d803e87f9d4fe2f440a82d3de6e7d1abeec7238d
Elliot Potter <elliotpotter496@gmail.com>
2023-08-26 16:17:09 -0400

    AgeRange gets internal state for proper warnings

--- src/components/account/AgeRange.tsx
+++ src/components/account/AgeRange.tsx
@@ -1,5 +1,6 @@
 import RangeSlider from "rsuite/RangeSlider";
 import "rsuite/dist/rsuite.css"
+import {useState} from "react";
 
 interface PropTypes {
     userAge: number
@@ -9,6 +10,8 @@ interface PropTypes {
 }
 
 export function AgeRange(props: PropTypes) {
+    const [internalAgeRange, setInternalAgeRange] = useState(props.default)
+
     return (
         <div style={{padding: 10}}>
             <div style={{fontSize: 12, marginBottom: 10}}>
@@ -25,12 +28,13 @@ export function AgeRange(props: PropTypes) {
                     renderMark={mark => mark}
                     constraint={([start, end]) => (start >= 18 && end >= 18)}
                     onChange={(val) => {
-                        props.onChange({min: val[0], max: val[1]}
-                    )}}
+                        props.onChange({min: val[0], max: val[1]})
+                        setInternalAgeRange({min: val[0], max: val[1]})
+                    }}
                 />
                 {
                     // if the user age is outside of the age range, warn them that is illegal
-                    (props.userAge > props.default.max || props.userAge < props.default.min) &&
+                    (props.userAge > internalAgeRange.max || props.userAge < internalAgeRange.min) &&
                     <div style={{color: "red", fontSize: 12}}>
                         Your age range must include your own age ({props.userAge})
                     </div>
