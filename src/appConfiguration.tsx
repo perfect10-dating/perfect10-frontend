dbcfbcc3ede2fb4758d4c11ac8d27cb8280a4455
Elliot Potter <elliotpotter496@gmail.com>
2023-08-28 12:48:28 -0400

    add contact email to appConfig

--- src/appConfiguration.tsx
+++ src/appConfiguration.tsx
@@ -4,6 +4,7 @@ const testApp = {
         ClientId: '2n6hgl70qm8but7chh0t52ngv8',
     },
     baseApiUrl: "http://localhost:3001/api",
+    supportEmail: "askrizzly@gmail.com",
 }
 
 const liveApp = {
@@ -12,6 +13,7 @@ const liveApp = {
         ClientId: '1hm0bcej0ok1rimjt93ehuuft3',
     },
     baseApiUrl: "https://api.rizz.ly/api",
+    supportEmail: "askrizzly@gmail.com",
     // baseApiUrl: "http://rizzly-dev-api.us-east-1.elasticbeanstalk.com/api",
 }
 
