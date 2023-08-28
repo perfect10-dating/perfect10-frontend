27db4d8ccdc2634479f132571eb76327913e4c36
theorymillar <50500565+theorymillar@users.noreply.github.com>
2023-08-28 13:20:46 -0400

    Added dynamic title to @PriorityModePage

--- src/components/premium/PriorityModePage.tsx
+++ src/components/premium/PriorityModePage.tsx
@@ -1,56 +1,84 @@
-import {useGetUserQuery} from "../../services/api";
-import {Loading} from "@minchat/react-chat-ui";
-import {useNavigate} from "react-router-dom";
-import {setUser} from "../../services/userSlice";
-import {useAppDispatch} from "../../app/hooks";
-import {LoadingWrapper} from "../misc/LoadingWrapper";
+import { useGetUserQuery } from "../../services/api";
+import { Loading } from "@minchat/react-chat-ui";
+import { useNavigate } from "react-router-dom";
+import { setUser } from "../../services/userSlice";
+import { useAppDispatch } from "../../app/hooks";
+import { LoadingWrapper } from "../misc/LoadingWrapper";
+import usePageTitle from "utils/usePageTitle";
 
 export function PriorityModePage() {
-    const dispatch = useAppDispatch()
-    const navigate = useNavigate()
+	const dispatch = useAppDispatch();
+	const navigate = useNavigate();
 
-    const {
-        data: user,
-        isLoading: userIsLoading,
-        isSuccess: userReqSuccessful,
-        isError: userReqFailed,
-        error: userReqError
-    } = useGetUserQuery()
+	usePageTitle("Rizzly | Priority Mode");
 
-    // if no user, we'll pop back to the "/" route, which will handle login
-    if (userReqFailed) {
-        console.log("PRIORITY_MODE: Failed getting user object, navigating to '/'")
-        dispatch(setUser({user: undefined}))
-        navigate("/")
-    }
+	const {
+		data: user,
+		isLoading: userIsLoading,
+		isSuccess: userReqSuccessful,
+		isError: userReqFailed,
+		error: userReqError,
+	} = useGetUserQuery();
 
-    if (!user) {
-        return <LoadingWrapper />
-    }
+	// if no user, we'll pop back to the "/" route, which will handle login
+	if (userReqFailed) {
+		console.log("PRIORITY_MODE: Failed getting user object, navigating to '/'");
+		dispatch(setUser({ user: undefined }));
+		navigate("/");
+	}
 
-    dispatch(setUser({user}))
-    return (
-        <div style={{display: "flex", justifyContent: "center", flexDirection: "column", height: "100vh"}}>
-            <div style={{maxWidth: "calc(100vw - 20)", maxHeight: "calc(100vh - 20)", textAlign: "center", margin: 10}}>
-                <p style={{fontSize: 30}}>Get Priority Mode!</p>
-                {(user.priorityMode && user.priorityModeExpiryTime) &&
-                    <p style={{marginTop: 10}}>
-                        Your Priority Mode expires on {(new Date(user.priorityModeExpiryTime)).toDateString()}
-                    </p>
-                }
-                <p style={{marginTop: 10}}>Members with Priority Mode have shorter wait times to get into rooms.</p>
-                <p style={{marginTop: 10}}>This might be helpful for you if there are many people with your identity and
-                sexual orientation trying to join a room with a few other members.</p>
-                <p style={{marginTop: 10}}>Priority Mode will be a paid feature in the future, but you can get it right now
-                by referring your friends!</p>
-                <p style={{marginTop: 10}}>Each person you refer gives you 30 days of Priority Mode</p>
-                <p style={{marginTop: 10}}>Just use this referral link!</p>
-                <p style={{marginTop: 10, textDecoration: 'underline', color: 'blue'}}>
-                    {window.location.href.replace('priority','') +
-                        `referral/${user._id.toString()}`}
-                </p>
+	if (!user) {
+		return <LoadingWrapper />;
+	}
 
-            </div>
-        </div>
-    )
+	dispatch(setUser({ user }));
+	return (
+		<div
+			style={{
+				display: "flex",
+				justifyContent: "center",
+				flexDirection: "column",
+				height: "100vh",
+			}}
+		>
+			<div
+				style={{
+					maxWidth: "calc(100vw - 20)",
+					maxHeight: "calc(100vh - 20)",
+					textAlign: "center",
+					margin: 10,
+				}}
+			>
+				<p style={{ fontSize: 30 }}>Get Priority Mode!</p>
+				{user.priorityMode && user.priorityModeExpiryTime && (
+					<p style={{ marginTop: 10 }}>
+						Your Priority Mode expires on{" "}
+						{new Date(user.priorityModeExpiryTime).toDateString()}
+					</p>
+				)}
+				<p style={{ marginTop: 10 }}>
+					Members with Priority Mode have shorter wait times to get into rooms.
+				</p>
+				<p style={{ marginTop: 10 }}>
+					This might be helpful for you if there are many people with your
+					identity and sexual orientation trying to join a room with a few other
+					members.
+				</p>
+				<p style={{ marginTop: 10 }}>
+					Priority Mode will be a paid feature in the future, but you can get it
+					right now by referring your friends!
+				</p>
+				<p style={{ marginTop: 10 }}>
+					Each person you refer gives you 30 days of Priority Mode
+				</p>
+				<p style={{ marginTop: 10 }}>Just use this referral link!</p>
+				<p
+					style={{ marginTop: 10, textDecoration: "underline", color: "blue" }}
+				>
+					{window.location.href.replace("priority", "") +
+						`referral/${user._id.toString()}`}
+				</p>
+			</div>
+		</div>
+	);
 }
