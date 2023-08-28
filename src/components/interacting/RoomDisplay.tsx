e4af463f8425711dcf7ce0f181c6379aab8dadce
theorymillar <50500565+theorymillar@users.noreply.github.com>
2023-08-28 17:33:33 -0400

    Changed to ProfileInformation instead of ProfileInRoom to display users. User will be able to click on any profile and the ProfileInRoom component will pop out and gray out the background

--- src/components/interacting/RoomDisplay.tsx
+++ src/components/interacting/RoomDisplay.tsx
@@ -1,35 +1,63 @@
-import {getOtherUserInDate, ProfileInRoom, userInDate} from "./ProfileInRoom";
+import { getOtherUserInDate, ProfileInRoom, userInDate } from "./ProfileInRoom";
+import { ProfileInformation } from "./ProfileInformation";
 
 interface PropTypes {
-    isDisplayingCompetitors: boolean
-    potentialPartners: UserMini[]
-    competitors: UserMini[]
-    dates: Date[]
-    conversations: Conversation[]
+	isDisplayingCompetitors: boolean;
+	potentialPartners: UserMini[];
+	competitors: UserMini[];
+	dates: Date[];
+	conversations: Conversation[];
 }
 
 export function RoomDisplay(props: PropTypes) {
-    return (
-        <div style={{overflowX: "auto", maxHeight: "100vh - 100px", maxWidth: "100vw",
-            display: "flex", flexDirection: "row", position: "relative",
-        }}>
-            {
-                (props.isDisplayingCompetitors ? props.competitors : props.potentialPartners).map(
-                    (person, key) => {
-                        return (
-                            <ProfileInRoom
-                                key={key}
-                                isCompetitor={props.isDisplayingCompetitors}
-                                information={person}
-                                potentialMatchedUsers={props.isDisplayingCompetitors ?
-                                                        props.potentialPartners : props.competitors}
-                                conversations={props.conversations}
-                                dates={props.dates}
-                            />
-                        )
-                    }
-                )
-            }
-        </div>
-    )
+	return (
+		<div
+			style={{
+				maxWidth: "100vw",
+				display: "inline-grid",
+				gridTemplateColumns:
+					"repeat(auto-fit, minmax(min(100%, max(250px, 100%/5)), auto))",
+				position: "relative",
+				justifyItems: "center",
+				paddingLeft: "4vw",
+				paddingRight: "4vw",
+			}}
+		>
+			{(props.isDisplayingCompetitors
+				? props.competitors
+				: props.potentialPartners
+			).map((person, key) => {
+				return (
+					<div
+						style={{
+							height: 300,
+							width: 225,
+							minWidth: 225,
+							marginBottom: 25,
+							position: "relative",
+						}}
+					>
+						<ProfileInformation
+							isInCardDeck={true}
+							scaleFontSize={3 / 4}
+							information={person}
+							distance={person.distance || 0}
+						></ProfileInformation>
+					</div>
+					// <ProfileInRoom
+					// 	key={key}
+					// 	isCompetitor={props.isDisplayingCompetitors}
+					// 	information={person}
+					// 	potentialMatchedUsers={
+					// 		props.isDisplayingCompetitors
+					// 			? props.potentialPartners
+					// 			: props.competitors
+					// 	}
+					// 	conversations={props.conversations}
+					// 	dates={props.dates}
+					// />
+				);
+			})}
+		</div>
+	);
 }
