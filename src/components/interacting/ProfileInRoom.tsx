31066973e65d267854e0feed81421601527ca8a2
theorymillar <50500565+theorymillar@users.noreply.github.com>
2023-08-28 17:32:52 -0400

    Formatting and fixed one double =

--- src/components/interacting/ProfileInRoom.tsx
+++ src/components/interacting/ProfileInRoom.tsx
@@ -1,175 +1,204 @@
-import {useEffect, useState} from "react";
-import {ProfileInformation} from "./ProfileInformation";
-import {ProfileMessages} from "./ProfileMessages";
-import {ProfileInteractions} from "./ProfileInteractions";
-import {ProfileTopBar} from "./ProfileTopBar";
-import {store} from "../../app/store";
+import { useEffect, useState } from "react";
+import { ProfileInformation } from "./ProfileInformation";
+import { ProfileMessages } from "./ProfileMessages";
+import { ProfileInteractions } from "./ProfileInteractions";
+import { ProfileTopBar } from "./ProfileTopBar";
+import { store } from "../../app/store";
 
 interface PropTypes {
-    isCompetitor: boolean
-    potentialMatchedUsers: UserMini[]
-    information: UserMini
-    conversations: Conversation[]
-    dates: Date[]
+	isCompetitor: boolean;
+	potentialMatchedUsers: UserMini[];
+	information: UserMini;
+	conversations: Conversation[];
+	dates: Date[];
 }
 
 export function userInDate(date: Date, userId: string) {
-    if (userId+"" === date.setupResponsibleUser+"") {
-        return true
-    }
-    for (let userIdInArr of date.users) {
-        if (userIdInArr+"" === userId+"") {
-            return true
-        }
-    }
+	if (userId + "" === date.setupResponsibleUser + "") {
+		return true;
+	}
+	for (let userIdInArr of date.users) {
+		if (userIdInArr + "" === userId + "") {
+			return true;
+		}
+	}
 }
 
 /**
  * Returns the ID, or user object, of the other user in the date
  */
 export function getOtherUserInDate(date: Date, userId: string) {
-    let setupResponsibleUserId = date.setupResponsibleUser
-    if (userId+"" !== setupResponsibleUserId+"") {
-        return date.setupResponsibleUser
-    }
-    for (let userInArr of date.users) {
-        // case -- the id is a string
-        if (userInArr+"" === userId+"") {
-            return userInArr
-        }
-    }
+	let setupResponsibleUserId = date.setupResponsibleUser;
+	if (userId + "" !== setupResponsibleUserId + "") {
+		return date.setupResponsibleUser;
+	}
+	for (let userInArr of date.users) {
+		// case -- the id is a string
+		if (userInArr + "" === userId + "") {
+			return userInArr;
+		}
+	}
 }
 
 export function ProfileInRoom(props: PropTypes) {
-    // can be "information", "messages", "interactions"
-    const [screenSetting, setScreenSetting] = useState("information")
-    const {"user": ownUserState} = store.getState()
-    let ownUser = ownUserState.user
+	// can be "information", "messages", "interactions"
+	const [screenSetting, setScreenSetting] = useState("information");
+	const { user: ownUserState } = store.getState();
+	let ownUser = ownUserState.user;
 
-    // if important props change (rendering a new user) display their pics
-    useEffect(() => {
-        setScreenSetting("information")
-    }, [props.isCompetitor, props.information._id])
+	// if important props change (rendering a new user) display their pics
+	useEffect(() => {
+		setScreenSetting("information");
+	}, [props.isCompetitor, props.information._id]);
 
-    let date = undefined
-    for (let possibleDate of props.dates) {
-        // check to see if both the logged in user and the user for this profile are in this date
-        if (ownUser && userInDate(possibleDate, props.information._id) && userInDate(possibleDate, ownUser._id)) {
-            // if date is undefined, define it
-            if (!date) {
-                date = possibleDate
-            }
-        }
-    }
+	let date = undefined;
+	for (let possibleDate of props.dates) {
+		// check to see if both the logged in user and the user for this profile are in this date
+		if (
+			ownUser &&
+			userInDate(possibleDate, props.information._id) &&
+			userInDate(possibleDate, ownUser._id)
+		) {
+			// if date is undefined, define it
+			if (!date) {
+				date = possibleDate;
+			}
+		}
+	}
 
-    let screenComponent
-    switch (screenSetting) {
-        case "information":
-            screenComponent = (props.information.photoLinks && props.information.photoLinks.length >= 1) ?
-                <ProfileInformation information={props.information} distance={props.information.distance || 0} /> :
-                <div>Invalid Profile</div>
-            break
-        case "messages":
-            screenComponent = <ProfileMessages otherUser={props.information} />
-            break
-        case "interactions":
-            screenComponent = <ProfileInteractions otherUser={props.information} date={date} />
-            break
-        default:
-            screenComponent = <div />
-    }
+	let screenComponent;
+	switch (screenSetting) {
+		case "information":
+			screenComponent =
+				props.information.photoLinks &&
+				props.information.photoLinks.length >= 1 ? (
+					<ProfileInformation
+						information={props.information}
+						distance={props.information.distance || 0}
+					/>
+				) : (
+					<div>Invalid Profile</div>
+				);
+			break;
+		case "messages":
+			screenComponent = <ProfileMessages otherUser={props.information} />;
+			break;
+		case "interactions":
+			screenComponent = (
+				<ProfileInteractions otherUser={props.information} date={date} />
+			);
+			break;
+		default:
+			screenComponent = <div />;
+	}
 
-    // ======================== BEGIN: Date interactions =======================
-    // does a competitor have a date set up with this profile?
-    let competitorHasDateWithProfile = false
-    let competitorDateIsSetup = false
-    let competitorDate: Date
-    let competitorId: string | undefined = undefined
-    let competitor: UserMini | undefined = undefined
+	// ======================== BEGIN: Date interactions =======================
+	// does a competitor have a date set up with this profile?
+	let competitorHasDateWithProfile = false;
+	let competitorDateIsSetup = false;
+	let competitorDate: Date;
+	let competitorId: string | undefined = undefined;
+	let competitor: UserMini | undefined = undefined;
 
-    // loops over dates and sees if any accepted dates with this user
-    for (let date of props.dates) {
-        if (!date || !date.isAccepted) {
-            continue
-        }
-        for (let i = 0; i < date.users.length; i++) {
-            if (date.users[i] === props.information._id) {
-                competitorHasDateWithProfile = true
-                competitorDate = date
+	// loops over dates and sees if any accepted dates with this user
+	for (let date of props.dates) {
+		if (!date || !date.isAccepted) {
+			continue;
+		}
+		for (let i = 0; i < date.users.length; i++) {
+			if (date.users[i] === props.information._id) {
+				competitorHasDateWithProfile = true;
+				competitorDate = date;
 
-                // the date is a setup, and the competitor set it up
-                if (date.users.length === 1) {
-                    competitorDateIsSetup = true
-                    competitorId = date.setupResponsibleUser
-                }
-                else {
-                    // the competitor is the other user
-                    competitorId = (date.users[i==0 ? 1 : 0] as string)
-                }
+				// the date is a setup, and the competitor set it up
+				if (date.users.length === 1) {
+					competitorDateIsSetup = true;
+					competitorId = date.setupResponsibleUser;
+				} else {
+					// the competitor is the other user
+					competitorId = date.users[i === 0 ? 1 : 0] as string;
+				}
 
-                break
-            }
-        }
-        // if this user set up the date
-        if (date.setupResponsibleUser === props.information._id) {
-            competitorHasDateWithProfile = true
-            competitorDate = date
-            competitorDateIsSetup = true
-            competitorId = date.users[0]
-        }
-        if (competitorHasDateWithProfile) {
-            break
-        }
-    }
+				break;
+			}
+		}
+		// if this user set up the date
+		if (date.setupResponsibleUser === props.information._id) {
+			competitorHasDateWithProfile = true;
+			competitorDate = date;
+			competitorDateIsSetup = true;
+			competitorId = date.users[0];
+		}
+		if (competitorHasDateWithProfile) {
+			break;
+		}
+	}
 
-    // there is a competing user that already planned a date with this profile
-    if (competitorId) {
-        for (let tempCompetitor of props.potentialMatchedUsers){
-            if (tempCompetitor._id === competitorId) {
-                competitor = tempCompetitor
-                break
-            }
-        }
-    }
+	// there is a competing user that already planned a date with this profile
+	if (competitorId) {
+		for (let tempCompetitor of props.potentialMatchedUsers) {
+			if (tempCompetitor._id === competitorId) {
+				competitor = tempCompetitor;
+				break;
+			}
+		}
+	}
 
-    // ===================== BEGIN: Conversation Information ====================
-    let messagesUnread = false
-    // loop over all conversations; find the one that references this profile
-    for (let conversation of props.conversations) {
-        for (let i = 0; i < conversation.users.length; i++) {
-            if (conversation.users[i] === props.information._id) {
-                // NOTE -- our user (ownUser) is the user at the OTHER index
-                if (i === 0 && !conversation.user1Read) {
-                    messagesUnread = true
-                }
-                if (i === 1 && !conversation.user0Read) {
-                    messagesUnread = true
-                }
-            }
-        }
-    }
+	// ===================== BEGIN: Conversation Information ====================
+	let messagesUnread = false;
+	// loop over all conversations; find the one that references this profile
+	for (let conversation of props.conversations) {
+		for (let i = 0; i < conversation.users.length; i++) {
+			if (conversation.users[i] === props.information._id) {
+				// NOTE -- our user (ownUser) is the user at the OTHER index
+				if (i === 0 && !conversation.user1Read) {
+					messagesUnread = true;
+				}
+				if (i === 1 && !conversation.user0Read) {
+					messagesUnread = true;
+				}
+			}
+		}
+	}
 
-    return (
-        <div style={{height: 400, width: 300, minWidth: 300, margin: 50, position: "relative", display: "inline-block"}}>
-            {
-                competitor &&
-                <div style={{height: "100%", width: "100%", borderRadius: 15,
-                    position: "absolute", bottom: 0, zIndex: 50,
-                    backgroundColor: "red", color: "white",
-                }}>
-                    {props.information.firstName} agreed to a {
-                        competitorDateIsSetup ? "setup" : "date"
-                    } with {competitor.firstName}{competitorDateIsSetup && `'s friend`}
-                </div>
-            }
-            {!props.isCompetitor &&
-                <ProfileTopBar
-                    screenSetting={screenSetting}
-                    setScreenSetting={setScreenSetting}
-                    markMessagesUnread={messagesUnread}
-                    markInteractionsUnread={!!date}
-                />}
-            {screenComponent}
-        </div>
-    )
+	return (
+		<div
+			style={{
+				height: 400,
+				width: 300,
+				minWidth: 300,
+				margin: 50,
+				position: "relative",
+				display: "inline-block",
+			}}
+		>
+			{competitor && (
+				<div
+					style={{
+						height: "100%",
+						width: "100%",
+						borderRadius: 15,
+						position: "absolute",
+						bottom: 0,
+						zIndex: 50,
+						backgroundColor: "red",
+						color: "white",
+					}}
+				>
+					{props.information.firstName} agreed to a{" "}
+					{competitorDateIsSetup ? "setup" : "date"} with {competitor.firstName}
+					{competitorDateIsSetup && `'s friend`}
+				</div>
+			)}
+			{!props.isCompetitor && (
+				<ProfileTopBar
+					screenSetting={screenSetting}
+					setScreenSetting={setScreenSetting}
+					markMessagesUnread={messagesUnread}
+					markInteractionsUnread={!!date}
+				/>
+			)}
+			{screenComponent}
+		</div>
+	);
 }
--- src/components/interacting/ProfileInformation.tsx
+++ src/components/interacting/ProfileInformation.tsx
@@ -1,109 +1,176 @@
-import {useState} from "react";
-import {fixIdentity} from "../../utils/fixIdentity";
+import { useState } from "react";
+import { fixIdentity } from "../../utils/fixIdentity";
 
 interface PropTypes {
-    information: UserMini
-    isPreview?: boolean
-    isInCardDeck?: boolean          // if isInCardDeck, don't display top grey / white bars, or nav features
-    scaleFontSize?: number          // scaleFontSize scales all fonts
-    distance: number
+	information: UserMini;
+	isPreview?: boolean;
+	isInCardDeck?: boolean; // if isInCardDeck, don't display top grey / white bars, or nav features
+	scaleFontSize?: number; // scaleFontSize scales all fonts
+	distance: number;
 }
 
 export function ProfileInformation(props: PropTypes) {
-    const [photoLinkIndex, setPhotoLinkIndex] = useState(0)
+	const [photoLinkIndex, setPhotoLinkIndex] = useState(0);
 
-    const scaleFont = props.scaleFontSize || 1
+	const scaleFont = props.scaleFontSize || 1;
 
-    return (
-        <div style={{height: "100%", width: "100%"}}>
-            {
-                !props.isInCardDeck &&
-                <div style={{position: "absolute", width: "100%"}}>
-                    <div style={{margin: 10, display: "flex", justifyContent: "space-evenly"}}>
-                        {
-                            props.information.photoLinks.map((link, key) => {
-                                return (
-                                    <div
-                                        key={key}
-                                        style={{
-                                            backgroundColor: key === photoLinkIndex ? "white" : "grey",
-                                            width: `${(100 / props.information.photoLinks.length) - 2}%`,
-                                            height: 5,
-                                            borderRadius: 2.5,
-                                        }} />
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            }
+	return (
+		<div style={{ height: "100%", width: "100%" }}>
+			{!props.isInCardDeck && (
+				<div style={{ position: "absolute", width: "100%" }}>
+					<div
+						style={{
+							margin: 10,
+							display: "flex",
+							justifyContent: "space-evenly",
+						}}
+					>
+						{props.information.photoLinks.map((link, key) => {
+							return (
+								<div
+									key={key}
+									style={{
+										backgroundColor: key === photoLinkIndex ? "white" : "grey",
+										width: `${100 / props.information.photoLinks.length - 2}%`,
+										height: 5,
+										borderRadius: 2.5,
+									}}
+								/>
+							);
+						})}
+					</div>
+				</div>
+			)}
 
-            <div style={{position: "absolute", height: "15%", width: "40%", bottom: 0, left: "5%",
-                color: "white", zIndex: 30
-            }}>
-                <div style={{display: "flex"}}>
-                    <div style={{fontSize: 24 * scaleFont, fontWeight: "semibold"}}>{props.information.firstName}</div>
-                    <div style={{fontSize: 20 * scaleFont, marginLeft: "8%", marginTop: "4%"}}>{props.information.age}</div>
-                </div>
+			<div
+				style={{
+					position: "absolute",
+					height: "15%",
+					width: "40%",
+					bottom: 0,
+					left: "5%",
+					color: "white",
+					zIndex: 30,
+				}}
+			>
+				<div style={{ display: "flex" }}>
+					<div style={{ fontSize: 24 * scaleFont, fontWeight: "semibold" }}>
+						{props.information.firstName}
+					</div>
+					<div
+						style={{
+							fontSize: 20 * scaleFont,
+							marginLeft: "8%",
+							marginTop: "4%",
+						}}
+					>
+						{props.information.age}
+					</div>
+				</div>
 
-                <div style={{marginTop: -3, fontSize: 12 * scaleFont, marginLeft: 5}}>
-                    {props.distance} miles away
-                </div>
-            </div>
+				<div style={{ marginTop: -3, fontSize: 12 * scaleFont, marginLeft: 5 }}>
+					{props.distance} miles away
+				</div>
+			</div>
 
-            <div style={{position: "absolute", height: "15%", width: "40%", bottom: 0, right: "5%",
-                color: "white", textAlign: "right", justifyContent: "right",
-                zIndex: 30
-            }}>
-                <div style={{fontSize: 20 * scaleFont, marginLeft: "5%", marginTop: "5%"}}>
-                    {fixIdentity(props.information.identity)}
-                </div>
+			<div
+				style={{
+					position: "absolute",
+					height: "15%",
+					width: "40%",
+					bottom: 0,
+					right: "5%",
+					color: "white",
+					textAlign: "right",
+					justifyContent: "right",
+					zIndex: 30,
+				}}
+			>
+				<div
+					style={{
+						fontSize: 20 * scaleFont,
+						marginLeft: "5%",
+						marginTop: "5%",
+					}}
+				>
+					{fixIdentity(props.information.identity)}
+				</div>
 
-                <div style={{marginTop: -3, fontSize: 12 * scaleFont, marginRight: 5}}>
-                    {props.information.shortTerm ? "Short-term" : "Long-term"}
-                </div>
-            </div>
+				<div
+					style={{ marginTop: -3, fontSize: 12 * scaleFont, marginRight: 5 }}
+				>
+					{props.information.shortTerm ? "Short-term" : "Long-term"}
+				</div>
+			</div>
 
-            {
-                (!props.isInCardDeck && photoLinkIndex !== 0) &&
-                <div style={{position: "absolute", height: "75%", width: "30%",
-                    color: "white", top: "10%", left: 0, fontSize: 60, cursor: "pointer",
-                    display: "flex", flexDirection: "column", justifyContent: "center",
-                    zIndex: 3
-                }}
-                onClick={() => setPhotoLinkIndex(photoLinkIndex-1)}
-                >
-                    <div style={{marginLeft: 15}}>
-                        &#8249;
-                    </div>
-                </div>
-            }
+			{!props.isInCardDeck && photoLinkIndex !== 0 && (
+				<div
+					style={{
+						position: "absolute",
+						height: "75%",
+						width: "30%",
+						color: "white",
+						top: "10%",
+						left: 0,
+						fontSize: 60,
+						cursor: "pointer",
+						display: "flex",
+						flexDirection: "column",
+						justifyContent: "center",
+						zIndex: 3,
+					}}
+					onClick={() => setPhotoLinkIndex(photoLinkIndex - 1)}
+				>
+					<div style={{ marginLeft: 15 }}>&#8249;</div>
+				</div>
+			)}
 
-            {
-                (!props.isInCardDeck && photoLinkIndex !== props.information.photoLinks.length-1) &&
-                <div style={{position: "absolute", height: "75%", width: "30%",
-                    color: "white", top: "10%", right: 0, fontSize: 60, cursor: "pointer",
-                    display: "flex", flexDirection: "column", justifyContent: "center",
-                    textAlign: "right", zIndex: 3
-                }}
-                     onClick={() => setPhotoLinkIndex(photoLinkIndex+1)}
-                >
-                    <div style={{marginRight: 15}}>
-                        &#8250;
-                    </div>
-                </div>
-            }
+			{!props.isInCardDeck &&
+				photoLinkIndex !== props.information.photoLinks.length - 1 && (
+					<div
+						style={{
+							position: "absolute",
+							height: "75%",
+							width: "30%",
+							color: "white",
+							top: "10%",
+							right: 0,
+							fontSize: 60,
+							cursor: "pointer",
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							textAlign: "right",
+							zIndex: 3,
+						}}
+						onClick={() => setPhotoLinkIndex(photoLinkIndex + 1)}
+					>
+						<div style={{ marginRight: 15 }}>&#8250;</div>
+					</div>
+				)}
 
-            <div style={{height: "50%", width: "100%", borderRadius: 15,
-                position: "absolute", bottom: 0, zIndex: 1,
-                background: "linear-gradient(#0000, #000)"
-            }}
-            />
+			<div
+				style={{
+					height: "50%",
+					width: "100%",
+					borderRadius: 15,
+					position: "absolute",
+					bottom: 0,
+					zIndex: 1,
+					background: "linear-gradient(#0000, #000)",
+				}}
+			/>
 
-            <img style={{height: "100%", width: "100%", borderRadius: 15,
-                border: "1px solid lightgray"}}
-                 src={props.information.photoLinks[photoLinkIndex] || ''}
-            />
-        </div>
-    )
+			<img
+				style={{
+					height: "100%",
+					width: "100%",
+					borderRadius: 15,
+					border: "1px solid lightgray",
+				}}
+				alt=""
+				src={props.information.photoLinks[photoLinkIndex] || ""}
+			/>
+		</div>
+	);
 }
