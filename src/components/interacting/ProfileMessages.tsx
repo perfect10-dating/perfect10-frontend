28f781646a1fb7c49f07e5ebcfa2fff34565df40
theorymillar <50500565+theorymillar@users.noreply.github.com>
2023-08-28 13:18:59 -0400

    Changed import from minchat to local component @ProfileMessages

--- src/components/interacting/ProfileMessages.tsx
+++ src/components/interacting/ProfileMessages.tsx
@@ -1,78 +1,101 @@
-import { MessageContainer } from "@minchat/react-chat-ui";
-import {useGetMessagesQuery, usePostMessageMutation} from "../../services/api";
-import {userSlice} from "../../services/userSlice";
-import {store} from "../../app/store";
-import {useEffect} from "react";
+import {
+	useGetMessagesQuery,
+	usePostMessageMutation,
+} from "../../services/api";
+import { userSlice } from "../../services/userSlice";
+import { store } from "../../app/store";
+import { useEffect } from "react";
+import { MessageContainer } from "./reactChat/src";
 // https://github.com/MinChatHQ/react-chat-ui
-const REFRESH_NUM_SECS = 10
+const REFRESH_NUM_SECS = 10;
 
 interface PropTypes {
-    otherUser: UserMini,
+	otherUser: UserMini;
 }
 
 export function ProfileMessages(props: PropTypes) {
-    const {"user": ownUserState} = store.getState()
-    let ownUser = ownUserState.user
-    let {data: messages, refetch: refetchMessages} = useGetMessagesQuery({
-        otherUserId: props.otherUser._id
-    })
+	const { user: ownUserState } = store.getState();
+	let ownUser = ownUserState.user;
+	let { data: messages, refetch: refetchMessages } = useGetMessagesQuery({
+		otherUserId: props.otherUser._id,
+	});
 
-    // starts a polling loop
-    useEffect(() => {
-        const loop = async () => {
-            while (true) {
-                await new Promise(resolve => setTimeout(resolve, REFRESH_NUM_SECS*1000))
-                refetchMessages()
-            }
-        }
+	// starts a polling loop
+	useEffect(() => {
+		const loop = async () => {
+			while (true) {
+				await new Promise((resolve) =>
+					setTimeout(resolve, REFRESH_NUM_SECS * 1000)
+				);
+				refetchMessages();
+			}
+		};
 
-        loop()
+		loop();
+	}, []);
 
-    }, [])
+	let [postMessage] = usePostMessageMutation();
+	let selectedConversation = {};
 
-    let [postMessage] = usePostMessageMutation()
-    let selectedConversation = {}
+	if (ownUser) {
+		selectedConversation = {
+			// TODO -- allow image messages
+			messages: (messages || [])
+				.filter((message) => message.text)
+				.map((message) => {
+					let info = {
+						firstName: ownUser?.firstName,
+						avatar: ownUser?.photoLinks[0],
+					};
+					if (message.sender === props.otherUser._id) {
+						info = {
+							firstName: props.otherUser.firstName,
+							avatar: props.otherUser.photoLinks[0],
+						};
+					}
 
-    if (ownUser) {
-        selectedConversation = {
-            // TODO -- allow image messages
-            messages: (messages || []).filter(message => message.text).map(message => {
-                let info = {firstName: ownUser?.firstName, avatar: ownUser?.photoLinks[0]}
-                if (message.sender === props.otherUser._id) {
-                    info = {firstName: props.otherUser.firstName, avatar: props.otherUser.photoLinks[0]}
-                }
+					return {
+						text: message.text,
+						user: {
+							avatar: info.avatar,
+							id: message.sender,
+							name: info.firstName,
+						},
+					};
+				}),
+			currentUserId: ownUser?._id,
+			onSendMessage: (message: string) => {
+				postMessage({
+					conversationId: (messages || [])[0]?.conversation || "",
+					otherUserId: props.otherUser._id,
+					text: message,
+					isImage: false,
+				});
 
-                return ({
-                    text: message.text,
-                    user: {
-                        avatar: info.avatar,
-                        id: message.sender,
-                        name: info.firstName
-                    }
-                })
-            }),
-            currentUserId: ownUser?._id,
-            onSendMessage: (message: string) => {
-                postMessage({
-                    conversationId: ((messages || [])[0]?.conversation || ""),
-                    otherUserId: props.otherUser._id,
-                    text: message,
-                    isImage: false
-                })
+				// refetchMessages()
+			},
 
-                // refetchMessages()
-            }
+			// TODO -- set up attachment features
+		};
+	}
 
-            // TODO -- set up attachment features
-        }
-    }
-
-    return (
-        <div style={{width: "100%", height: "100%", zIndex: 0, position: "relative"}}>
-            <div style={{position: "absolute", zIndex: 1000, display: "flex", justifyContent: "space-evenly",
-                width: "100%", marginTop: 18, backgroundColor: "rgb(243,244,246)", paddingBottom: 18,}}
-                />
-            <MessageContainer {...selectedConversation} mobileView={true} />
-        </div>
-    )
+	return (
+		<div
+			style={{ width: "100%", height: "100%", zIndex: 0, position: "relative" }}
+		>
+			<div
+				style={{
+					position: "absolute",
+					zIndex: 1000,
+					display: "flex",
+					justifyContent: "space-evenly",
+					width: "100%",
+					marginTop: 18,
+					backgroundColor: "rgb(243,244,246)",
+					paddingBottom: 18,
+				}}
+			/>
+			<MessageContainer {...selectedConversation} mobileView={true} />
+		</div>
+	);
 }
