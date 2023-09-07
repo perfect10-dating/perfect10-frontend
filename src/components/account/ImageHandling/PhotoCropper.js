c1b19f73ddc217261438aca7874950b46698cd79
theorymillar <50500565+theorymillar@users.noreply.github.com>
2023-09-07 14:12:43 -0400

    Fixed marginTop to show all text

--- src/components/account/ImageHandling/PhotoCropper.js
+++ src/components/account/ImageHandling/PhotoCropper.js
@@ -1,197 +1,225 @@
-import 'cropperjs/dist/cropper.css'
-import { Component } from 'react'
-import Cropper from 'react-cropper'
-import { v4 as uuid } from 'uuid'
-import * as imageConversion from 'image-conversion';
+import "cropperjs/dist/cropper.css";
+import { Component } from "react";
+import Cropper from "react-cropper";
+import { v4 as uuid } from "uuid";
+import * as imageConversion from "image-conversion";
 
-import styled from 'styled-components/macro'
+import styled from "styled-components/macro";
 
-import { useGetS3SignedUrlMutation } from 'services/api'
+import { useGetS3SignedUrlMutation } from "services/api";
 
 const withHooks = (PhotoCropper) => (props) => {
-    const [getSignedUrl] = useGetS3SignedUrlMutation()
+	const [getSignedUrl] = useGetS3SignedUrlMutation();
 
-    return <PhotoCropper {...props} getSignedUrl={getSignedUrl} />
-}
+	return <PhotoCropper {...props} getSignedUrl={getSignedUrl} />;
+};
 
 class PhotoCropper extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            saving: false,
-            compressing: false,
-            showUploadLoading: false,
-            imageSize: 0,
-        }
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			saving: false,
+			compressing: false,
+			showUploadLoading: false,
+			imageSize: 0,
+		};
+	}
 
-    dataURItoBlob = (dataURI, type) => {
-        type = type || 'image/png'
-        let binary = atob(dataURI.split(',')[1])
-        let array = []
-        for (let i = 0; i < binary.length; i++) {
-            array.push(binary.charCodeAt(i))
-        }
-        return new Blob([new Uint8Array(array)], { type: type })
-    }
+	dataURItoBlob = (dataURI, type) => {
+		type = type || "image/png";
+		let binary = atob(dataURI.split(",")[1]);
+		let array = [];
+		for (let i = 0; i < binary.length; i++) {
+			array.push(binary.charCodeAt(i));
+		}
+		return new Blob([new Uint8Array(array)], { type: type });
+	};
 
-    componentDidMount() {
-        this._isMounted = true // workaround since React has deprecated isMounted()
-    }
+	componentDidMount() {
+		this._isMounted = true; // workaround since React has deprecated isMounted()
+	}
 
-    componentWillUnmount() {
-        this._isMounted = false // workaround since React has deprecated isMounted()
-    }
+	componentWillUnmount() {
+		this._isMounted = false; // workaround since React has deprecated isMounted()
+	}
 
-    handleSave = async () => {
-        if (this.state.saving) return // don't interrupt current save operation
-        let dataUrl = this._cropper.cropper.getCroppedCanvas().toDataURL('image/png')
-        let blob = this.dataURItoBlob(dataUrl)
-        let file = new File([blob], uuid()) // randomize file name
+	handleSave = async () => {
+		if (this.state.saving) return; // don't interrupt current save operation
+		let dataUrl = this._cropper.cropper
+			.getCroppedCanvas()
+			.toDataURL("image/png");
+		let blob = this.dataURItoBlob(dataUrl);
+		let file = new File([blob], uuid()); // randomize file name
 
-        this.setState({ compressing: true, showUploadLoading: true, imageSize: (file.size / 1000).toFixed(1) })
+		this.setState({
+			compressing: true,
+			showUploadLoading: true,
+			imageSize: (file.size / 1000).toFixed(1),
+		});
 
-        let compressedFile
-        try {
-            // pass if the file is already smaller than 100 kb
-            if (file.size < 100000) {
-                compressedFile = file
-            }
-            else {
-                // compress the file down to 100 kb
-                compressedFile = await imageConversion.compressAccurately(file, {
-                    size: 100,
-                    accuracy: 0.8,
-                    type: file.type,
-                    width: 300,
-                    height: 400
-                })
-            }
-        }
-        catch (err) {
-            console.error(err)
-            alert("Saving failed-- error with compression. Please try again")
-            return
-        }
+		let compressedFile;
+		try {
+			// pass if the file is already smaller than 100 kb
+			if (file.size < 100000) {
+				compressedFile = file;
+			} else {
+				// compress the file down to 100 kb
+				compressedFile = await imageConversion.compressAccurately(file, {
+					size: 100,
+					accuracy: 0.8,
+					type: file.type,
+					width: 300,
+					height: 400,
+				});
+			}
+		} catch (err) {
+			console.error(err);
+			alert("Saving failed-- error with compression. Please try again");
+			return;
+		}
 
-        this.setState({ saving: true, showUploadLoading: true, imageSize: (compressedFile.size / 1000).toFixed(1) })
+		this.setState({
+			saving: true,
+			showUploadLoading: true,
+			imageSize: (compressedFile.size / 1000).toFixed(1),
+		});
 
-        this.props
-            .getSignedUrl()
-            .then(async (value) => {
-                if (value?.data?.signedRequest) {
-                    await fetch(value.data.signedRequest, {
-                        method: 'Put',
-                        headers: {
-                            'content-type': compressedFile.type,
-                        },
-                        body: compressedFile,
-                    })
-                    this.props.onSave(value.data.resourceUrl)
-                }
-            })
-            .catch(() => {
-                alert(
-                    'The image could not be cropped. Please check your internet ' +
-                    'connection and make sure your clock is set to the correct time.'
-                )
-            })
-            .finally(() => {
-                this.setState({ saving: false })
-            })
-    }
+		this.props
+			.getSignedUrl()
+			.then(async (value) => {
+				if (value?.data?.signedRequest) {
+					await fetch(value.data.signedRequest, {
+						method: "Put",
+						headers: {
+							"content-type": compressedFile.type,
+						},
+						body: compressedFile,
+					});
+					this.props.onSave(value.data.resourceUrl);
+				}
+			})
+			.catch(() => {
+				alert(
+					"The image could not be cropped. Please check your internet " +
+						"connection and make sure your clock is set to the correct time."
+				);
+			})
+			.finally(() => {
+				this.setState({ saving: false });
+			});
+	};
 
-    loadPhoto = (photoUrl) => {
-        return new Promise((resolve, reject) => {
-            const img = new Image()
-            img.onload = () => resolve(img)
-            img.onerror = (...args) => reject(args)
-            img.src = photoUrl
-        })
-    }
+	loadPhoto = (photoUrl) => {
+		return new Promise((resolve, reject) => {
+			const img = new Image();
+			img.onload = () => resolve(img);
+			img.onerror = (...args) => reject(args);
+			img.src = photoUrl;
+		});
+	};
 
-    getImageData = (image) => {
-        const canvas = document.createElement('canvas')
-        canvas.width = image.width
-        canvas.height = image.height
-        const context2d = canvas.getContext('2d')
-        context2d.drawImage(image, 0, 0)
-        return context2d.getImageData(0, 0, image.width, image.height)
-    }
+	getImageData = (image) => {
+		const canvas = document.createElement("canvas");
+		canvas.width = image.width;
+		canvas.height = image.height;
+		const context2d = canvas.getContext("2d");
+		context2d.drawImage(image, 0, 0);
+		return context2d.getImageData(0, 0, image.width, image.height);
+	};
 
-    renderUploadLoading = () => {
-        return (
-            <div>
-                <div className="overlay-background"></div>
-                <div className="photo-cropper-overlay">
-                    <div>
-                        <div className="loading-box">
-                            {/*<img src="/img/standard_icons/waiting.png" alt="Loading"></img>*/}
-                            <p>Uploading photo ({this.state.imageSize} kb)...</p>
-                            <p>For optimal performance, please use photo smaller than 512kb</p>
-                        </div>
-                        <CropSubmit style={{
-                            backgroundColor: 'rgb(181, 181, 181)', cursor: 'default',
-                            marginTop: 15, padding: 2, paddingLeft: 5, paddingRight: 5,
-                            borderRadius: 5, color: "black"
-                        }}>
-                            {this.state.saving ? "Saving..." : "Compressing..."}
-                        </CropSubmit>
-                        <br />
-                        <p className="crop-cancel" onClick={this.props.onCancel}>
-                            Cancel
-                        </p>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+	renderUploadLoading = () => {
+		return (
+			<div>
+				<div className="overlay-background"></div>
+				<div className="photo-cropper-overlay">
+					<div>
+						<div className="loading-box">
+							{/*<img src="/img/standard_icons/waiting.png" alt="Loading"></img>*/}
+							<p>Uploading photo ({this.state.imageSize} kb)...</p>
+							<p>
+								For optimal performance, please use photo smaller than 512kb
+							</p>
+						</div>
+						<CropSubmit
+							style={{
+								backgroundColor: "rgb(181, 181, 181)",
+								cursor: "default",
+								marginTop: 15,
+								padding: 2,
+								paddingLeft: 5,
+								paddingRight: 5,
+								borderRadius: 5,
+								color: "black",
+							}}
+						>
+							{this.state.saving ? "Saving..." : "Compressing..."}
+						</CropSubmit>
+						<br />
+						<p className="crop-cancel" onClick={this.props.onCancel}>
+							Cancel
+						</p>
+					</div>
+				</div>
+			</div>
+		);
+	};
 
-    render() {
-        let aspectRatio = 1
-        if (this.props.aspectRatio) {
-            aspectRatio = this.props.aspectRatio
-        }
-        return (
-            <div style={{color: "lightgray", textAlign: "center", marginTop: 10}}>
-                {this.state.showUploadLoading ? this.renderUploadLoading() :
-
-                    <div>
-                        <p>Drag and resize the blue box to crop the photo</p>
-                        <p>When you're satisfied with your selection, click 'Save'</p>
-                        <Cropper
-                            src={this.props.src}
-                            style={{ height: 400, width: 400, margin: '20px auto', maxWidth: "80vw", maxHeight: "60vh" }}
-                            initialAspectRatio={aspectRatio}
-                            aspectRatio={this.props.lockAspectRatio ? aspectRatio : null}
-                            guides={false}
-                            ref={(cropper) => {
-                                this._cropper = cropper
-                            }}
-                        />
-                        <div style={{display: "flex", fontSize: 24, justifyContent: "space-evenly"}}>
-                            <CropSubmit onClick={this.handleSave}>
-                                Save
-                            </CropSubmit>
-                            <CropCancel onClick={this.props.onCancel}>Cancel</CropCancel>
-                        </div>
-                    </div>
-                }
-            </div>
-        )
-    }
+	render() {
+		let aspectRatio = 1;
+		if (this.props.aspectRatio) {
+			aspectRatio = this.props.aspectRatio;
+		}
+		return (
+			<div style={{ color: "lightgray", textAlign: "center", marginTop: 65 }}>
+				{this.state.showUploadLoading ? (
+					this.renderUploadLoading()
+				) : (
+					<div>
+						<p>Drag and resize the blue box to crop the photo</p>
+						<p>When you're satisfied with your selection, click 'Save'</p>
+						<Cropper
+							src={this.props.src}
+							style={{
+								height: 400,
+								width: 400,
+								margin: "20px auto",
+								maxWidth: "80vw",
+								maxHeight: "60vh",
+							}}
+							initialAspectRatio={aspectRatio}
+							aspectRatio={this.props.lockAspectRatio ? aspectRatio : null}
+							guides={false}
+							ref={(cropper) => {
+								this._cropper = cropper;
+							}}
+						/>
+						<div
+							style={{
+								display: "flex",
+								fontSize: 24,
+								justifyContent: "space-evenly",
+							}}
+						>
+							<CropSubmit onClick={this.handleSave}>Save</CropSubmit>
+							<CropCancel onClick={this.props.onCancel}>Cancel</CropCancel>
+						</div>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
 
-export default withHooks(PhotoCropper)
+export default withHooks(PhotoCropper);
 
 const CropSubmit = styled.div`
-  display: inline-block;
-  color: white;
-  cursor: pointer;
-`
+	display: inline-block;
+	color: white;
+	cursor: pointer;
+`;
 
 const CropCancel = styled.div`
-  display: inline-block;
-  cursor: pointer;
-  color: lightgray;
-`
+	display: inline-block;
+	cursor: pointer;
+	color: lightgray;
+`;
